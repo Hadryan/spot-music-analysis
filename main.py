c5eb01932c7d88fe9f452ca455f39668d37fb86f
Xander Joosse <xmjoosse@gmail.com>
2020-05-13 18:36:00 +0200

    Baisc  next beat detection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from multiprocessing import Process, Queue
+import time
 import tekore as tk 
 import numpy as np 
-from multiprocessing import Process, Queue
+
 
 
 def _load_config(filename):
@@ -28,21 +30,88 @@ def validate_credentials(filename='config.ini'):
     return token
 
 
+def find_previous_section(time_stamp, start_times):
+    """
+    Returns the index fo the previous beat/section/....  in the list
+    """
+    # Kan nog versneld worden door items te poppen die al voorbij zijn, 
+    # maar als je terugspoelt moeten die weer terug gehaald worden...
+    return np.argmin(np.abs(np.array([i for i in start_times if i < time_stamp])-time_stamp))
+
+
+def _set_audio_data():
+    global beats
+    global sections
+    global audio
+    
+    # create list of start times for fast loops
+    beats = [float(beat.start) for beat in audio.beats]
+    sections = [float(section.start) for section in audio.sections]
+    print(f"beats {len(beats)}, sections {len(sections)}")
 
 
 
+def beat_detection(progressed_time):
+    """
+    progressed_time (s) and finds next beat using find_previous_section()
+    """
+    global audio
+    global beats
 
 
-def spotify_analysis(spotify_object):
+    start = time.time()
+    next_beat = find_previous_section(progressed_time,beats) + 1
+    time_to_beat = audio.beats[next_beat].start - progressed_time
+    end = time.time()
+    diff = end - start
+    time.sleep(time_to_beat - diff)
+    print(f"next beat {next_beat}/{len(beats)}, time to next beat = {time_to_beat}, diff {diff}", end="\r")
+
+    # return time.sleep(1)
+
+
+
+def spotify_analysis(spotify):
     """
     Main loop for the program, finds beats and sections
     and sends items to the queue 
     """
 
-    print("Starting Main loop")
+    global audio
+    previous = None
 
-    return
+    while spotify.playback():
+        if spotify.playback().is_playing:
+            current = spotify.playback_currently_playing() #deze wordt te vaak opgevraagd passed time moet dat voorkomen lijkt te werken
 
+            if current.item.id != previous:
+                audio = spotify.track_audio_analysis(current.item.id)
+                _set_audio_data()
+                print(f"Song:{current.item.name} - Artist(s):{[a.name for a in current.item.artists]}", end='\n')
+                previous = current.item.id
+                
+            else:
+                passed_time = 0
+                progress = current.progress_ms
+                # print("Update real progress time")
+                
+                while passed_time < 2:
+                    # Start timing of process
+                    interval_start = time.time()
+
+                    # feedback going to be adjusted
+                    # print(f"Progress: ({progress+passed_time*1000}/{audio.track['duration']*1000}) passed time {passed_time}", end='\r')
+                    
+                    # Beat detections --> artnet connection
+                    beat_detection(progress/1000+passed_time) 
+ 
+                    # Time the duration and adjust progress
+                    interval_end = time.time()
+                    passed_time += interval_end-interval_start
+            
+        else:
+            # If no spotify playback sleep 1s and try again
+            time.sleep(1)
 
 
 
@@ -56,9 +125,6 @@ def artnet_control():
 
 
 
-
-
-
 if __name__ == "__main__":
 
     # Try to initialise spotify
@@ -68,8 +134,12 @@ if __name__ == "__main__":
     except:
         AssertionError("Failed to initialise spotify")
 
-    # setup a queue 
+    # setup a queue for process communication
     queque = Queue()
+
+    # set global var where current audio data is stored
+    # this is global because needs to be used in multiple processes
+    audio,beats,sections = None,None,None
 
     # Define the 2 processes
     main_process = Process(target=spotify_analysis, args=[spotify])
